e2bba2a7d17da52f177196dd6f3b111ff7c93c95
caohao <705083979@qq.com>
2017-04-19 14:55:33 +0800

    start

---
+++ src/main/java/MyApp/Application.java
@@ -0,0 +1,15 @@
+package MyApp;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by xdcao on 2017/4/19.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args){
+
+    }
+
+}
